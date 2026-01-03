83bfca21172fba5d7c6f25890dee2415f5369732
pavi-191004 <bpavithra4646@gmail.com>
2026-01-03 16:25:00 +0530

    first commit

---
+++ src/Error/Errorhandling.jsx
@@ -0,0 +1,22 @@
+export const user = {
+  name: "Pavithra",
+  address: {
+    city: "Chennai"
+  },
+  profile: {
+    email: "pavi@gmail.com"
+  }
+};
+const email = user.profile?.email ?? "Email not available";
+const phone = user.profile?.phone ?? "Phone not available";
+return(
+    <>
+    {user.address?.city}
+{user.address?.pincode}
+{email}
+{phone}
+    </>
+  
+
+)
+
---
+++ src/Form/Form.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+
+export const Form = () => {
+
+  const [data, setData] = useState({
+    Name: "",
+    age: "",
+    email: "",
+    city: ""
+  });
+
+  const [errors, setErrors] = useState({});
+
+
+  const validate = () => {
+    const newErrors = {};
+
+    if (data.Name.trim() === "") {
+      newErrors.Name = "Name is required";
+    }
+
+    if (data.age.trim() === "") {
+  newErrors.age = "Age is required";
+    } else if (Number(data.age) || Number(data.age) <= 0) {
+ newErrors.age = "Age must be a valid number";
+    }
+
+    if (data.email.trim() === "") {
+      newErrors.email = "Email is required";
+    } else if ((!data.email.includes("@")) ) {
+newErrors.email = "Invalid email format";
+    }
+
+    if (data.city.trim() === "") {
+      newErrors.city = "City is required";
+    }
+
+    return newErrors;
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const validationErrors = validate();
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+alert("Form submitted successfully ");
+      console.log(data);
+    }
+  };
+
+ 
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setData((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  return (
+    <>
+      <form className="form-fields" onSubmit={handleSubmit}>
+
+        <label>Name:</label>
+        <input
+          name="Name"
+  value={data.Name}
+          onChange={handleOnChange}
+        />
+ {errors.Name && <p style={{ color: "red" }}>{errors.Name}</p>}
+
+        <label>Age:</label>
+        <input
+          name="age"
+          value={data.age}
+          onChange={handleOnChange}
+        />
+        {errors.age && <p style={{ color: "red" }}>{errors.age}</p>}
+
+        <label>Email:</label>
+<input
+          name="email"
+          value={data.email}
+          onChange={handleOnChange}
+        />
+        {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
+
+        <label>City:</label>
+<input
+          name="city"
+          value={data.city}
+          onChange={handleOnChange}
+        />
+        {errors.city && <p style={{ color: "red" }}>{errors.city}</p>}
+
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
